c04d6f842f80f12e251e1f3850624422c95f3124
Cole Lawrence <colelawr@gmail.com>
2019-03-04 11:17:36 -0500

    Make sure .trigger file exists

--- dev-watch.sh
+++ dev-watch.sh
@@ -4,5 +4,6 @@
 # cargo install systemfd cargo-watch
 
 # from https://actix.rs/docs/autoreload/
+touch '.trigger'
 cargo watch -i .trigger -w src -w templates -x build -s 'touch .trigger' &
 systemfd --no-pid -s http::8088 -- cargo watch -w .trigger -x "run start"
