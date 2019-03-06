df58d1c5c9e0e566c435017ed1379adb4f05a23f
Cole Lawrence <colelawr@gmail.com>
2019-03-06 17:58:57 -0500

    Add example form values migrations

---
+++ migrations/__archive/2019-03-06-162437_add_forms/down.sql
@@ -0,0 +1,7 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE form_nested_forms;
+DROP FUNCTION form_has_property;
+DROP TABLE form_default_choice_properties;
+DROP TABLE form_properties;
+DROP FUNCTION form_properties_ord;
+DROP TABLE forms;
